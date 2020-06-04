0b20e4aba8f62a56e3a15133fc261f4e2c7dd3da
Raph40 <rafajoselopes@gmail.com>
2020-06-04 11:20:19 +0100

    First commit

---
+++ helo.py
@@ -0,0 +1,4 @@
+def say_hi():
+    print('Hi, my name is Rafael Lopes.')
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Rafael',
+      author_email='rafajoselopes@gmail.com',
+      url='The URL project - github for example',
+      py_modules=['hello'],
+     )
